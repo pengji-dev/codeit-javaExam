bed1566a574eb61b01f5c04ce556fe1b71bd79ea
kjh940222 <kjh940222@gmail.com>
2026-07-06 17:19:18 +0900

    실습 3-2: Conditional 조건문 = 중첩 if-else문 (안티 패턴)

--- src/main/java/com/codeit/javaExam/JavaExamApplication.java
+++ src/main/java/com/codeit/javaExam/JavaExamApplication.java
@@ -4,15 +4,15 @@ package com.codeit.javaExam;
 public class JavaExamApplication {
     public static void main(String[] args) {
 //		SpringApplication.run(JavaExamApplication.class, args);
-        int score = 85;
+        int age = 20;
+        boolean isStudent = true;
 
-        if (score >= 90) {
-            System.out.println("A학점");
-        } else if (score >= 80) {
-            System.out.println("B학점");
-        } else {
-            System.out.println("C 이하");
+        if (age >= 18) {
+            if (isStudent) {
+                System.out.println("성인 학생");
+            }
         }
+//        중첩 if-else문을 사용할 수 있으나 들여쓰기가 너무 많이 발생하기 때문에 안티패턴에 해당한다.
     }
 
 }
