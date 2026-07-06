7318603675e8bcc1c75ac0ccee2c1955eff8e4d8
kjh940222 <kjh940222@gmail.com>
2026-07-06 18:43:54 +0900

    실습 3-10: Loop 반복문 = 기본 while문

--- src/main/java/com/codeit/javaExam/JavaExamApplication.java
+++ src/main/java/com/codeit/javaExam/JavaExamApplication.java
@@ -5,11 +5,11 @@ public class JavaExamApplication {
     public static void main(String[] args) {
 //		SpringApplication.run(JavaExamApplication.class, args);
 
-//      향상된 for문 : 배열이나 JCF(Java Collection Framework) 다양한 자료구조로부터 하나씩 꺼내오는 방식
-        int[] scores = {90, 85, 78};
+        int countdown = 5;
 
-        for (int s : scores) {
-            System.out.println("점수 : " + s);
+//      while문 : 언제 끝낼지에 대한 조건을 설정해놓은 것을 바탕으로 루프를 돈다.
+        while (countdown > 0) {
+            System.out.println("countdown = " + countdown--);
         }
     }
 }
