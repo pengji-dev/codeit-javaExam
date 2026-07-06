a23eaa28ae8431e34e5e5e6a6949e7d470c86aeb
kjh940222 <kjh940222@gmail.com>
2026-07-06 16:24:44 +0900

    실습 1-6: 객체 .toString() 메서드 재정의(Override) 필요

--- src/main/java/com/codeit/javaExam/JavaExamApplication.java
+++ src/main/java/com/codeit/javaExam/JavaExamApplication.java
@@ -1,25 +1,9 @@
 package com.codeit.javaExam;
 
+import com.codeit.javaExam.member.Member;
+
 //@SpringBootApplication
 public class JavaExamApplication {
-    static class Member {
-        private Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL이 들어갈 수 없다.
-        private String name;
-        private int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL이 들어갈 수 없다. (무조건 값이 있어야 하기에 기본값 존재)
-        private String email;
-
-        public Member(String name, String email) {
-//        this.id = null;       // Reference Type이기 때문에 값을 설정하지 않으면 NULL이 들어간다.
-            this.name = name;
-//        this.age = 0;       // Primitive Type이기 때문에 값을 설정하지 않으면 기본값이 0이 된다.
-            this.email = email;
-        }
-
-        public String getName() {
-            return this.name;
-        }
-    }
-
     public static void main(String[] args) {
 //		SpringApplication.run(JavaExamApplication.class, args);
         Member aaron = new Member("Aaron", "aaron@example.com");
@@ -29,13 +13,13 @@ public class JavaExamApplication {
         System.out.println(aaron);                    // 객체
         System.out.println(aaron.toString());        // 객체
         System.out.println(aaron.getName());        // 필드 중 name
-        System.out.println(aaron.name);            // 필드 중 name 불가능 (접근제어자 private이지만, 내부(inner) 클래스에 속하면 접근 가능)
+//        System.out.println(aaron.name);            // 필드 중 name (접근제어자 private)
 
         System.out.println("---");
         System.out.println(baron);                    // 객체
         System.out.println(baron.toString());        // 객체
         System.out.println(baron.getName());        // 필드 중 name
-        System.out.println(baron.name);                // 필드 중 name 불가능 (접근제어자 private이지만, 내부(inner) 클래스에 속하면 접근 가능)
+//        System.out.println(baron.name);                // 필드 중 name (접근제어자 private)
     }
 
 }
