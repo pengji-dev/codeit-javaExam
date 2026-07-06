ee595398f0ed01b5b5a5f95eb2e82e2a0c5aef9a
kjh940222 <kjh940222@gmail.com>
2026-07-06 18:33:59 +0900

    실습 3-7: Loop 반복문 = 기본 for문

--- src/main/java/com/codeit/javaExam/JavaExamApplication.java
+++ src/main/java/com/codeit/javaExam/JavaExamApplication.java
@@ -1,30 +1,13 @@
 package com.codeit.javaExam;
 
-import java.util.Scanner;
-
 //@SpringBootApplication
 public class JavaExamApplication {
     public static void main(String[] args) {
 //		SpringApplication.run(JavaExamApplication.class, args);
-        Scanner sc = new Scanner(System.in);
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
 
-        System.out.println("요일을 입력하세요. ex) MONDAY");
-        String day = sc.nextLine();
-
-        String dayType = switch (day) {
-            case "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY" -> {
-                System.out.println("주중이다. 공부하자...");
-                yield "Weekday";
-            }
-            case "SATURDAY", "SUNDAY" -> {
-                System.out.println("주말이다. 놀자!!!");
-                yield "Weekend";
-            }
-            default -> "Unknown";
-        };    // 무언가 대입하는 식(Expression)이므로 끝에 세미콜론(;) 필수!
+//        일반 for문 : 시작이 'int i = 1;'부터면 종료는 'i <= 5;'가 됨. / 시작이 'int i = 0;'부터면 종료는 'i < 5;'가 됨.
     }
-//	Java 14 버전: break 쓰지 않고 간단하게 switch문 사용 가능하며 ':' 아닌 '->' 사용
-//  switch문 자체적으로 값 반환 가능 : switch문 각각 case 조건 안에서 바로 값 반환
-//  switch문에서 다수의 case 조건을 하나로 묶을 수 있음 : 다수의 조건을 ','으로 이으면 됨.
-//  case 조건 안에서 다양한 로직을 '{}'로 묶어내고 yield으로 최종 값 반환
 }
