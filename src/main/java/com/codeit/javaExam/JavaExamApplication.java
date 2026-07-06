db66cc90e607bdce0b01dfc6efeb1b530ff8b9a1
kjh940222 <kjh940222@gmail.com>
2026-07-06 16:13:05 +0900

    실습 1-4: Private 접근제어자 = 어디에서도 접근 불가능 + Getter

--- src/main/java/com/codeit/javaExam/JavaExamApplication.java
+++ src/main/java/com/codeit/javaExam/JavaExamApplication.java
@@ -14,13 +14,13 @@ public class JavaExamApplication {
         System.out.println(aaron);                // 객체
         System.out.println(aaron.toString());    // 객체
         System.out.println(aaron.getName());    // 필드 중 name
-        System.out.println(aaron.name);            // 필드 중 name 불가능 (접근제어자 public)
+//      System.out.println(aaron.name);            // 필드 중 name 불가능 (접근제어자 private)
 
         System.out.println("---");
         System.out.println(baron);                // 객체
         System.out.println(baron.toString());    // 객체
         System.out.println(baron.getName());    // 필드 중 name
-        System.out.println(baron.name);            // 필드 중 name 불가능 (접근제어자 public)
+//      System.out.println(baron.name);            // 필드 중 name 불가능 (접근제어자 private)
     }
 
 }
--- src/main/java/com/codeit/javaExam/member/Member.java
+++ src/main/java/com/codeit/javaExam/member/Member.java
@@ -1,10 +1,10 @@
 package com.codeit.javaExam.member;
 
 public class Member {
-    public Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL이 들어갈 수 없다.
-    public String name;
-    public int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL이 들어갈 수 없다. (무조건 값이 있어야 하기에 기본값 존재)
-    public String email;
+    private Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL이 들어갈 수 없다.
+    private String name;
+    private int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL이 들어갈 수 없다. (무조건 값이 있어야 하기에 기본값 존재)
+    private String email;
 
     public Member(String name, String email) {
 //        this.id = null;       // Reference Type이기 때문에 값을 설정하지 않으면 NULL이 들어간다.
