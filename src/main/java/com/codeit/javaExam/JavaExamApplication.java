e1092b0d9f27ccbeaf3351e890176a6d29c935a8
kjh940222 <kjh940222@gmail.com>
2026-07-06 15:10:56 +0900

    실습 1-1: 클래스의 기본 3요소(셍성자 + 필드 + 메서드)

--- src/main/java/com/codeit/javaExam/JavaExamApplication.java
+++ src/main/java/com/codeit/javaExam/JavaExamApplication.java
@@ -6,9 +6,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@SpringBootApplication
 public class JavaExamApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 //		SpringApplication.run(JavaExamApplication.class, args);
-		System.out.println("Hello, World!!!");
-	}
+        Member aaron = new Member("Aaron", "aaron@example.com");
+        Member baron = new Member("Baron", "baron@example.com");
+
+        System.out.println("---");
+        System.out.println(aaron);                // 객체
+        System.out.println(aaron.toString());    // 객체
+        System.out.println(aaron.getName());    // 필드 중 name
+        System.out.println(aaron.name);            // 필드 중 name (접근제어자 default)
+
+        System.out.println("---");
+        System.out.println(baron);                // 객체
+        System.out.println(baron.toString());    // 객체
+        System.out.println(baron.getName());    // 필드 중 name
+        System.out.println(baron.name);            // 필드 중 name (접근제어자 default)
+    }
 
 }
