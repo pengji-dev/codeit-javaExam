aacce24fa58c498015afc98dc9e1661c3804d6f8
kjh940222 <kjh940222@gmail.com>
2026-07-06 17:22:31 +0900

    실습 3-3: Conditional 조건문 = 중첩 if-else문 (가독성과 비즈니스 로직 상 명료성 고려)

--- src/main/java/com/codeit/javaExam/JavaExamApplication.java
+++ src/main/java/com/codeit/javaExam/JavaExamApplication.java
@@ -7,12 +7,11 @@ public class JavaExamApplication {
         int age = 20;
         boolean isStudent = true;
 
-        if (age >= 18) {
-            if (isStudent) {
-                System.out.println("성인 학생");
-            }
+        if (age >= 18 && isStudent) {
+            System.out.println("성인 학생");
         }
 //        중첩 if-else문을 사용할 수 있으나 들여쓰기가 너무 많이 발생하기 때문에 안티패턴에 해당한다.
+//        가독성과 비즈니스 로직 상 명료성을 고려해서 작성해야 한다.
     }
 
 }
