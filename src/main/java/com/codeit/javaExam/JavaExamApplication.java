bf39737850a95a47043aa49c84925e59827cc5e8
kjh940222 <kjh940222@gmail.com>
2026-07-06 17:15:51 +0900

    실습 3-1: Conditional 조건문 = if-else문 기본 형태

--- src/main/java/com/codeit/javaExam/JavaExamApplication.java
+++ src/main/java/com/codeit/javaExam/JavaExamApplication.java
@@ -1,26 +1,18 @@
 package com.codeit.javaExam;
 
-import com.codeit.javaExam.member.Administrator;
-import com.codeit.javaExam.member.Member;
-
 //@SpringBootApplication
 public class JavaExamApplication {
     public static void main(String[] args) {
 //		SpringApplication.run(JavaExamApplication.class, args);
-        Administrator aaron = new Administrator("Aaron", "aaron@example.com", "DEVELOPER");
-        Administrator baron = new Administrator("Baron", "baron@example.com", "MANAGER");
+        int score = 85;
 
-        System.out.println("---");
-        System.out.println(aaron);                    // 객체
-        System.out.println(aaron.toString());        // 객체
-        System.out.println(aaron.getName());        // 필드 중 name
-//        System.out.println(aaron.name);            // 필드 중 name (접근제어자 private)
-
-        System.out.println("---");
-        System.out.println(baron);                    // 객체
-        System.out.println(baron.toString());        // 객체
-        System.out.println(baron.getName());        // 필드 중 name
-//        System.out.println(baron.name);                // 필드 중 name (접근제어자 private)
+        if (score >= 90) {
+            System.out.println("A학점");
+        } else if (score >= 80) {
+            System.out.println("B학점");
+        } else {
+            System.out.println("C 이하");
+        }
     }
 
 }
