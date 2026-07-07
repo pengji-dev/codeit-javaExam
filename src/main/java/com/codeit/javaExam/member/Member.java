80f9076b52325f70a259eb7a2dbc08dfc94d22e1
kjh940222 <kjh940222@gmail.com>
2026-07-07 13:45:10 +0900

    실습 2-9: @ToString.Exclude : 미출력 필드 제외

--- src/main/java/com/codeit/javaExam/member/Member.java
+++ src/main/java/com/codeit/javaExam/member/Member.java
@@ -3,14 +3,16 @@ package com.codeit.javaExam.member;
 import lombok.*;
 
 @Getter
-@ToString(exclude = {"age", "email"})                // exclude 옵션 : 미출력 필드 제외
+@ToString
 @AllArgsConstructor(access = AccessLevel.PUBLIC)     // 생성자의 접근 제어자 옵션 설정 가능
 public class Member {
     private Integer id;
     private String name;
     @Setter(AccessLevel.PRIVATE)
+    @ToString.Exclude               // @ToString.Exclude : 미출력 필드 제외
     private int age;
     @Setter
+    @ToString.Exclude               // @ToString.Exclude : 미출력 필드 제외
     private String email;
 
 //    public Member(Integer id, String name, int age, String email) {
