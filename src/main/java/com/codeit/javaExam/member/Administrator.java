6a1e809df8a9f1f906233872fd7cf3479c98be72
kjh940222 <kjh940222@gmail.com>
2026-07-07 16:14:29 +0900

    실습 2-14: @tostring - 클래스 상속 시 자식 클래스의 필드만 노출 (이슈)

--- src/main/java/com/codeit/javaExam/member/Administrator.java
+++ src/main/java/com/codeit/javaExam/member/Administrator.java
@@ -1,10 +1,11 @@
 package com.codeit.javaExam.member;
 
 import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
-//@AllArgsConstructor       // 클래스 상속 시, @AllArgsConstructor 사용 불가 -> 자식 클래스 내부에서 부모 클래스의 필드까지 포함하는 생성자를 자동으로 생성하면 super()를 올바르게 호출하지 못한다.
+@ToString           // 클래스 상속 시, 자식 클래스에 @ToString 사용하면 자식 클래스의 필드만 출력된다.
 public class Administrator extends Member {
     private String role;
     private LocalDate allocatedAt;
@@ -15,10 +16,10 @@ public class Administrator extends Member {
         this.allocatedAt = LocalDate.now();
     }
 
-    public String toString() {
-        return String.format(
-                "Administrator(id=%s, name=%s, age=%s, email=%s, role=%s, allocatedAt=%s)",
-                super.getId(), super.getName(), super.getAge(), super.getEmail(), this.role, this.allocatedAt
-        );
-    }
+//    public String toString() {
+//        return String.format(
+//                "Administrator(id=%s, name=%s, age=%s, email=%s, role=%s, allocatedAt=%s)",
+//                super.getId(), super.getName(), super.getAge(), super.getEmail(), this.role, this.allocatedAt
+//        );
+//    }
 }
