ec8894184c90ddd6abc21bca7631ee9d75f67181
kjh940222 <kjh940222@gmail.com>
2026-07-07 13:39:18 +0900

    실습 2-7: @tostring - 객체 내 필드들을 한번에 출력

--- src/main/java/com/codeit/javaExam/member/Member.java
+++ src/main/java/com/codeit/javaExam/member/Member.java
@@ -1,11 +1,9 @@
 package com.codeit.javaExam.member;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
+@ToString
 @AllArgsConstructor(access = AccessLevel.PUBLIC)     // 생성자의 접근 제어자 옵션 설정 가능
 public class Member {
     private Integer id;
@@ -14,10 +12,6 @@ public class Member {
     private int age;
     @Setter
     private String email;
-
-    public String toString() {
-        return String.format("Member(id=%s, name=%s, age=%s, email=%s)", id, name, age, email);
-    }
 
 //    public Member(Integer id, String name, int age, String email) {
 //        this.id = id;
