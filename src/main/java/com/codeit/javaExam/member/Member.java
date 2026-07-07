0b4d2dbf5fd2bc4920ba8d9458b83a43506e9037
kjh940222 <kjh940222@gmail.com>
2026-07-07 13:49:13 +0900

    실습 2-10: @tostring - of 옵션 : 출력 필드 포함

--- src/main/java/com/codeit/javaExam/member/Member.java
+++ src/main/java/com/codeit/javaExam/member/Member.java
@@ -3,16 +3,14 @@ package com.codeit.javaExam.member;
 import lombok.*;
 
 @Getter
-@ToString
+@ToString(of = {"id", "name"})                       // of 옵션 : 출력 필드 포함
 @AllArgsConstructor(access = AccessLevel.PUBLIC)     // 생성자의 접근 제어자 옵션 설정 가능
 public class Member {
     private Integer id;
     private String name;
     @Setter(AccessLevel.PRIVATE)
-    @ToString.Exclude               // @ToString.Exclude : 미출력 필드 제외
     private int age;
     @Setter
-    @ToString.Exclude               // @ToString.Exclude : 미출력 필드 제외
     private String email;
 
 //    public Member(Integer id, String name, int age, String email) {
