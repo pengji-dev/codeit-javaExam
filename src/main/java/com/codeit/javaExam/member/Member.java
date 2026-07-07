6f718937c90580762a81903cc9cd8ffab55b7b03
kjh940222 <kjh940222@gmail.com>
2026-07-07 16:22:29 +0900

    실습 2-16: (심화)  @tostring - 필드명 굳이 같이 노출하기 싫을때 옵션

--- src/main/java/com/codeit/javaExam/member/Member.java
+++ src/main/java/com/codeit/javaExam/member/Member.java
@@ -3,8 +3,9 @@ package com.codeit.javaExam.member;
 import lombok.*;
 
 @Getter
-@ToString(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true, includeFieldNames = false)
 // onlyExplicitlyIncluded 옵션 : ture 시, @ToString.Include로 지정된 필드만 선택적으로 출력
+// includeFieldNames 옵션 : false 시, 지정된 필드명을 노출하고 싶지 않을 때 사용하는 옵션
 @AllArgsConstructor(access = AccessLevel.PUBLIC)     // 생성자의 접근 제어자 옵션 설정 가능
 public class Member {
     @ToString.Include(name = "memberId")            // name 옵션 : 출력 시 이름을
