07eb03b6ce47cbe58b9b8174d1a642355ecea2b8
kjh940222 <kjh940222@gmail.com>
2026-07-07 15:27:09 +0900

    실습 2-11: @ToString.Include + 명시적 포함 옵션 : 출력 필드 포함

--- src/main/java/com/codeit/javaExam/member/Member.java
+++ src/main/java/com/codeit/javaExam/member/Member.java
@@ -3,10 +3,13 @@ package com.codeit.javaExam.member;
 import lombok.*;
 
 @Getter
-@ToString(of = {"id", "name"})                       // of 옵션 : 출력 필드 포함
+@ToString(onlyExplicitlyIncluded = true)
+// onlyExplicitlyIncluded 옵션 : ture 시, @ToString.Include로 지정된 필드만 선택적으로 출력
 @AllArgsConstructor(access = AccessLevel.PUBLIC)     // 생성자의 접근 제어자 옵션 설정 가능
 public class Member {
+    @ToString.Include
     private Integer id;
+    @ToString.Include
     private String name;
     @Setter(AccessLevel.PRIVATE)
     private int age;
