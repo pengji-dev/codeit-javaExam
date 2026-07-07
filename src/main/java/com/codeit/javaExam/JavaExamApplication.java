89cb7fa65ded330dd201a4f930ebe40dd897266e
kjh940222 <kjh940222@gmail.com>
2026-07-07 13:10:05 +0900

    실습 2-4: @Setter - 클래스 내 필드 단위 적용

--- src/main/java/com/codeit/javaExam/JavaExamApplication.java
+++ src/main/java/com/codeit/javaExam/JavaExamApplication.java
@@ -13,6 +13,11 @@ public class JavaExamApplication {
         System.out.println(aaron);              // 객체
         System.out.println(aaron.toString());   // 객체
 
+//        aaron.setId();                        // @Setter 필드 단위로 설정되어 있지 않아 필드 수정 불가능
+//        aaron.setName();                      // @Setter 필드 단위로 설정되어 있지 않아 필드 수정 불가능
+        aaron.setAge(20);                       // @Setter (필드 단위)
+        aaron.setEmail("aaron@different.com");  // @Setter (필드 단위)
+
         System.out.println(aaron.getId());      // @Getter 클래스 단위로 적용하여 모든 필드 조회 가능
         System.out.println(aaron.getName());    // @Getter (클래스 단위)
         System.out.println(aaron.getAge());     // @Getter (클래스 단위)
--- src/main/java/com/codeit/javaExam/member/Member.java
+++ src/main/java/com/codeit/javaExam/member/Member.java
@@ -3,13 +3,16 @@ package com.codeit.javaExam.member;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PUBLIC)     // 생성자의 접근 제어자 옵션 설정 가능
 public class Member {
     private Integer id;
     private String name;
+    @Setter
     private int age;
+    @Setter
     private String email;
 
 //    public Member(Integer id, String name, int age, String email) {
