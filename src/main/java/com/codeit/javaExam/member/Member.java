ec063fdac21087c9a7141fbedf3da236f04f6a0e
kjh940222 <kjh940222@gmail.com>
2026-07-07 15:33:34 +0900

    실습 2-12: (심화, 잘안씀) @tostring 표기 순서 및 이름 지정

--- src/main/java/com/codeit/javaExam/member/Member.java
+++ src/main/java/com/codeit/javaExam/member/Member.java
@@ -7,9 +7,9 @@ import lombok.*;
 // onlyExplicitlyIncluded 옵션 : ture 시, @ToString.Include로 지정된 필드만 선택적으로 출력
 @AllArgsConstructor(access = AccessLevel.PUBLIC)     // 생성자의 접근 제어자 옵션 설정 가능
 public class Member {
-    @ToString.Include
+    @ToString.Include(name = "memberId")            // name 옵션 : 출력 시 이름을
     private Integer id;
-    @ToString.Include
+    @ToString.Include(rank = -1)                    // rank 옵션 : 필드들의 출력 순서를 제어 (기본값은 0) -> 기본값보다 뒤에 출력
     private String name;
     @Setter(AccessLevel.PRIVATE)
     private int age;
