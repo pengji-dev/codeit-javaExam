d94cd38eb43757f59b5d4bcc2addfc5851fa0167
kjh940222 <kjh940222@gmail.com>
2026-07-08 10:58:07 +0900

    실습 1-32: Static 메서드 - 객체 생성 후 호출하지 않고 클래스로 바로 호출

--- src/main/java/com/codeit/javaExam/JavaExamApplication.java
+++ src/main/java/com/codeit/javaExam/JavaExamApplication.java
@@ -15,5 +15,7 @@ public class JavaExamApplication {
         System.out.println(member.toString());                  // 객체
         System.out.println(member.MEMBER_TO_STRING_FORMAT);     // * 앞글자 소문자 : 물론 이렇게 호출할 수 있겠지만, 정상 사용은 아님.
         System.out.println(Member.MEMBER_TO_STRING_FORMAT);     // * 앞글자 대문자 : Static 필드는 이렇게 호출하는 것이 정상.
+        System.out.println(member.toString(member));
+        System.out.println(Member.toString(member));
     }
 }
--- src/main/java/com/codeit/javaExam/member/Member.java
+++ src/main/java/com/codeit/javaExam/member/Member.java
@@ -17,4 +17,10 @@ public class Member {
     public String toString() {
         return String.format(MEMBER_TO_STRING_FORMAT, this.id, this.name, this.age, this.email);
     }
+
+    public static String toString(Member member) {
+        return String.format(MEMBER_TO_STRING_FORMAT, member.id, member.name, member.age, member.email);
+    }
+    // Static 메서드 : 객체(인스턴스) 없이 사용 가능한 메서드
+    // 인스턴스 생성 없이 호출 가능하여, 유틸리티 관련 메서드를 만드는데 유용하게 사용 (ex. Objects.isNull() 등)
 }
